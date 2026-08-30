68e5c22626579b6391da1247277d5b9b17626de5
Maciej <maciej.wozniczka@outlook.com>
2026-08-30 12:31:36 +0200

    Added DataContext

---
+++ api/EventFinder.Infrastructure/Data/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using EventFinder.Domain.Activities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventFinder.Infrastructure.Data;
+
+public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+{
+    public required DbSet<Activity> Activities { get; set; }
+}
