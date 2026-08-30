c0618fdd8e04310bf7e7e168889bc6b583b0fd4f
Maciej <maciej.wozniczka@outlook.com>
2026-08-30 12:31:45 +0200

    Added Activity class

---
+++ api/EventFinder.Domain/Activities/Activity.cs
@@ -0,0 +1,18 @@
+﻿namespace EventFinder.Domain.Activities;
+
+public class Activity
+{
+    // Activity props
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public required string Title { get; set; }
+    public required DateTime Date { get; set; }
+    public required string Description { get; set; }
+    public required string Category { get; set; }
+    public bool IsCancelled { get; set; } = false;
+
+    // Location props
+    public required string City { get; set; }
+    public required string Venue { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
