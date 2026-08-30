6a6ca9513316f98f863631aebb0996fa3e4c17a3
Maciej <maciej.wozniczka@outlook.com>
2026-08-30 13:20:25 +0200

    Added Get Activities endpoints

---
+++ api/EventFinder.Api/Controllers/GetActivities.cs
@@ -0,0 +1,18 @@
+﻿using EventFinder.Domain.Activities;
+using EventFinder.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventFinder.Api.Controllers;
+
+[Route("api/activities")]
+[ApiController]
+public class GetActivities (DataContext db) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivitiesAsync(CancellationToken cancellationToken)
+    {
+        return await db.Activities
+            .ToListAsync(cancellationToken);
+    }
+}
---
+++ api/EventFinder.Api/Controllers/GetActivity.cs
@@ -0,0 +1,26 @@
+﻿using EventFinder.Domain.Activities;
+using EventFinder.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventFinder.Api.Controllers;
+
+[Route("api/activities")]
+[ApiController]
+public class GetActivity(DataContext db) : ControllerBase
+{
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivityAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var activity = await db.Activities
+            .Where(a => a.Id == id)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (activity == null)
+        {
+            return NotFound();
+        }
+
+        return activity;
+    }
+}
