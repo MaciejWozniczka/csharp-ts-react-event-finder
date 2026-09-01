dc774df5b9dc66712795709898ddf858358d2006
Maciej <maciej.wozniczka@outlook.com>
2026-09-01 17:31:26 +0200

    Added BaseApiController

---
+++ api/EventFinder.Api/Helper/BaseApiController.cs
@@ -0,0 +1,14 @@
+﻿namespace EventFinder.Api.Helper;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseApiController : ControllerBase
+{
+    private IMediator? _mediator;
+
+    protected CancellationToken CancellationToken => HttpContext.RequestAborted;
+
+    protected IMediator Mediator => 
+        _mediator ??= HttpContext.RequestServices.GetRequiredService<IMediator>() 
+                      ?? throw new InvalidOperationException("IMediator not available");
+}
