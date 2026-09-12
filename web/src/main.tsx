203e2c3aa900628507bfb0fb398148457828cd64
Maciej <maciej.wozniczka@outlook.com>
2026-09-12 18:15:31 +0200

    Corrected typo

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -6,7 +6,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import "react-toastify/dist/ReactTostify.css";
+import "react-toastify/dist/ReactToastify.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { router } from "./app/router/Routers.tsx";
