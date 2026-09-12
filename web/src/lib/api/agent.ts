d31fb84260ca8c785e9c83202f2df39f9ab53c4b
Maciej <maciej.wozniczka@outlook.com>
2026-09-12 18:12:14 +0200

    Added Toast notifications for error messages

--- web/src/lib/api/agent.ts
+++ web/src/lib/api/agent.ts
@@ -1,15 +1,33 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const agent = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-agent.interceptors.response.use(async (response) => {
-  try {
+agent.interceptors.response.use(
+  async (response) => {
     return response;
-  } catch (error) {
+  },
+  async (error) => {
+    const { status } = error.response;
+    switch (status) {
+      case 400:
+        toast.error("Niepoprawne zapytanie");
+        break;
+      case 401:
+        toast.error("Brak autoryzacji");
+        break;
+      case 404:
+        toast.error("Nie znaleziono elementu");
+        break;
+      case 500:
+        toast.error("Błąd serwera");
+        break;
+    }
+
     return Promise.reject(error);
-  }
-});
+  },
+);
 
 export default agent;
