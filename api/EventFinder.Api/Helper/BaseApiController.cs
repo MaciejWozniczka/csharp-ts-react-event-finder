1e7a42ab352bca200aa9d230a5736adc6ff39e9a
Maciej <maciej.wozniczka@outlook.com>
2026-09-12 13:47:41 +0200

    Added HandleResult method to BaseApiController

--- api/EventFinder.Api/Helper/BaseApiController.cs
+++ api/EventFinder.Api/Helper/BaseApiController.cs
@@ -5,10 +5,18 @@
 public class BaseApiController : ControllerBase
 {
     private IMediator? _mediator;
-
     protected CancellationToken CancellationToken => HttpContext.RequestAborted;
 
     protected IMediator Mediator => 
         _mediator ??= HttpContext.RequestServices.GetRequiredService<IMediator>() 
                       ?? throw new InvalidOperationException("IMediator not available");
+
+    protected ActionResult HandleResult<T>(Result<T> result)
+    {
+        if (!result.IsSuccess && result.Code == 404) return NotFound();
+
+        if (result.IsSuccess && result.Value != null) return Ok(result.Value);
+
+        return BadRequest(result.Error);
+    }
 }
