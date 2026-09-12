b847b792dd979ed3c16d581836137670e78119de
Maciej <maciej.wozniczka@outlook.com>
2026-09-12 14:34:42 +0200

    Added EditActivityValidator

---
+++ api/EventFinder.Application/Activities/Validators/EditActivityValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace EventFinder.Application.Activities.Validators;
+
+public class EditActivityValidator : BaseActivityValidator<EditActivity.Command, EditActivityDto>
+{
+    public EditActivityValidator() : base(x => x.ActivityDto)
+    {
+        RuleFor(x => x.ActivityDto.Id)
+            .NotEmpty().WithMessage("Id jest wymagane");
+    }
+}
