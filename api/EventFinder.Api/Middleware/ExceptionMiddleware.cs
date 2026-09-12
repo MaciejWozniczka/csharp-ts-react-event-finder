78994337e2ab1084fea41af70b8d764d15403da3
Maciej <maciej.wozniczka@outlook.com>
2026-09-12 14:07:09 +0200

    Added handling app exceptions

--- api/EventFinder.Api/Middleware/ExceptionMiddleware.cs
+++ api/EventFinder.Api/Middleware/ExceptionMiddleware.cs
@@ -1,6 +1,8 @@
-﻿namespace EventFinder.Api.Middleware;
+﻿using System.Text.Json;
 
-public class ExceptionMiddleware : IMiddleware
+namespace EventFinder.Api.Middleware;
+
+public class ExceptionMiddleware(ILogger<ExceptionMiddleware> logger, IHostEnvironment env) : IMiddleware
 {
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
@@ -14,8 +16,26 @@ public class ExceptionMiddleware : IMiddleware
         }
         catch (Exception ex)
         {
-            Console.WriteLine(ex);
+            await HandleException(context, ex);
         }
+    }
+
+    private async Task HandleException(HttpContext context, Exception ex)
+    {
+        logger.LogError(ex, ex.Message);
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+        var response = env.IsDevelopment()
+            ? new AppException(context.Response.StatusCode, ex.Message, ex.StackTrace)
+            : new AppException(context.Response.StatusCode, ex.Message, null);
+
+        var options = new JsonSerializerOptions{ PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+        var json = JsonSerializer.Serialize(response, options);
+
+        await context.Response.WriteAsync(json);
     }
 
     private static async Task HandleValidationException(HttpContext context, ValidationException ex)
