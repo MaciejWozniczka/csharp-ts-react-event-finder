5b41033dece9fd7ae5dcd5dcfde80d9585eab0fc
Maciej <maciej.wozniczka@outlook.com>
2026-09-12 14:42:16 +0200

    Added React Tostify for errors in web

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -6,10 +6,12 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import "react-toastify/dist/ReactTostify.min.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { router } from "./app/router/Routers.tsx";
 import { RouterProvider } from "react-router";
+import { ToastContainer } from "react-toastify";
 
 const queryClient = new QueryClient();
 
@@ -17,6 +19,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+      <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
       <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>,
